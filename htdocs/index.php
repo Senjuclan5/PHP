5d486c9e7fbf2092788316aa8177281957cfacfe
Senjuclan5 <88778429+Senjuclan5@users.noreply.github.com>
2022-02-07 14:42:47 -0500

    Add files via upload

---
+++ htdocs/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body> 
+<?php
+
+
+
+$firstName = "Side";//string https://youtu.be/97LnEncGx2c
+  $marks = 87; //integer  https://youtu.be/LJH-MZv0GOE
+$x = 13.5; //float https://youtu.be/d3c_OOD4Jzs
+echo $x;
+$isComplete = true; //boolean https://youtu.be/1kO_g_ucYCQ
+if ($isComplete) {
+    
+} else {
+    
+}
+
+$programmingLanguages = ['PHP', 'Java', 'Python']; //arrays https://youtu.be/C8ZFLq24g_A
+
+
+
+?>
+</body>
+</html>
